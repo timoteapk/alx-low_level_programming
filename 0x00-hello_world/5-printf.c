33de8f2260e9a165e662131c771a427bc6bbf498
timoteapk <timthyeffa@gmail.com>
2023-07-13 21:42:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line using the printf function
+ * return: 0(Sucessful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
