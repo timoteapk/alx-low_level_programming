e711cace67111137ff3f71244fbd8fc9e49e87d3
timoteapk <timthyeffa@gmail.com>
2023-07-14 09:27:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints exactly a text, to the standard error
+ * main - Entry point
  * return: 1 (Sucess)
  */
 int main(void)
 {
- fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	 stdout);
 return (1);
 }
