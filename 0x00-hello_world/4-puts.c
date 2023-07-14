b1aa3e9735903aedc6bfd06d74ea3cca571c157d
timoteapk <timthyeffa@gmail.com>
2023-07-14 08:47:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that prints exactly a text followed by a new line
+ * main - prints exactly a text followed by a new line
  * return: 0 (Sucessful)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return  (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return  (0);
 }
