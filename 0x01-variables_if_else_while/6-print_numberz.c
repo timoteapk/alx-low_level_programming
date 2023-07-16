31d2f5bb50a547e6a39ffe7651cef9001037404b
timoteapk <timthyeffa@gmail.com>
2023-07-16 01:41:54 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all single numbers of base ten from zero
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; <= 9; i++)
+	{
+		putchar("i + '0'\n");
+	}
+	return (0);
+}
