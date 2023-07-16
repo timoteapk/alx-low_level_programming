3e65660085a62b820080ece3578874de5ab35d42
timoteapk <timthyeffa@gmail.com>
2023-07-16 08:35:56 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,12 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
+		{
 			putchar(i + '0');
 			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
+		}
 	}
 	{
 		putchar('\n');
