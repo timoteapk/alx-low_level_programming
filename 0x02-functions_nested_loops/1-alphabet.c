cfcb980c0b9bcd6169c31f0802b6c8a91664bd40
timoteapk <timthyeffa@gmail.com>
2023-07-19 15:00:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * @void: print lowercase alphabetes
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
