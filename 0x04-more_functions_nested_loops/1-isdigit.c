833993ddf0a018da0d25e11c1aa71c91933aad85
timoteapk <timthyeffa@gmail.com>
2023-07-20 11:36:29 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isdigit - point of entry
+ *
+ * Return: 0 always
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
