2fcd6f01cbdb6455cf373bf9229d65370f16ba78
timoteapk <timthyeffa@gmail.com>
2023-07-19 17:46:32 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,9 +14,9 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
+	{
+		_putchar('-');
+		return (-1);
         }
 	else
 	{
