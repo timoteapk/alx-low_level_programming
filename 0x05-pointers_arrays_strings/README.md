d8cc0621a0045c723af2adcf17641f876712dcfe
timoteapk <timthyeffa@gmail.com>
2023-07-25 08:36:38 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+file contain pointers and arrays 
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.c"
+#include <unisdt.h>
+
+/**
+ * main - point of entry
+ *
+ * Return: 0 always
+ */
+
+int _putchar(char c)
+{
+	return write (1, &c, 1);
+}
