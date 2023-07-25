c534d7e20885faafb200334771ef30177ea8b370
timoteapk <timthyeffa@gmail.com>
2023-07-25 13:08:18 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of a statement
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int di = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		di++;
+		i++;
+	}
+	s--;
+	for (i = di; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
