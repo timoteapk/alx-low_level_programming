ecdf2605672cc9735348dfa544af529d21347515
timoteapk <timthyeffa@gmail.com>
2023-07-31 13:36:44 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
