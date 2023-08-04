447c8f3ae0e0b3d41b38ebe0ac11cbad34569df9
timoteapk <timthyeffa@gmail.com>
2023-08-04 08:52:17 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a programme for printing number of arguments
+ * @n: number of arguments
+ * @ar: number of arrays
+ *
+ * Result: successful (0)
+ */
+
+int main (int n, char *ar[])
+{
+	(void)ar;
+	printf ("%d\n", n - 1);
+
+	return (0);
+}
