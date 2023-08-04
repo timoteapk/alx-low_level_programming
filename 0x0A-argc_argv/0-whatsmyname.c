14614375eb05db0a6e35757ebfe7cb92fa779a39
timoteapk <timthyeffa@gmail.com>
2023-08-04 08:46:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - programe name input
+ * @argc: argument number
+ * @av: argument array
+ *
+ * Return: 0
+ */
+
+int main (int argc __attribute__((unused)), char *av[])
+{
+	printf("%s\n", *av);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MIAN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif
